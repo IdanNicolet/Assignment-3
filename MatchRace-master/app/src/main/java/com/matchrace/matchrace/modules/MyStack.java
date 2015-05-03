d46c21186b9f45c517ac52e20b10ec687974d935
Nehor Golan <nehorg@gmail.com>
2015-05-03 12:57:07 +0300

    Added new Synchronised Stack class

---
+++ MatchRace-master/app/src/main/java/com/matchrace/matchrace/modules/MyStack.java
@@ -0,0 +1,35 @@
+package com.matchrace.matchrace.modules;
+
+import java.util.Stack;
+
+/**
+ * Created by nehorg on 5/3/15.
+ */
+public class MyStack extends Stack<String>{
+    private Stack<String> st;
+
+    public MyStack()
+    {
+        st = new Stack<String>();
+    }
+
+    public synchronized String pop()
+    {
+        if (!isEmpty())
+            return st.pop();
+        return null;
+    }
+
+    public synchronized String push(String s)
+    {
+        st.push(s);
+        return null;
+    }
+
+    public synchronized boolean isEmpty()
+    {
+        return st.isEmpty();
+    }
+
+
+}
